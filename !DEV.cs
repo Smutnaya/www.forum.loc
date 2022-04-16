d684ccc5fd70a179676d527787eb261ce2dae5e0
Smutnaya <lana900033@gmail.com>
2022-04-16 17:19:31 +0300

    dev

---
+++ !DEV.cs
@@ -0,0 +1,8 @@
+$ composer create-project --prefer-dist laravel/laravel:7.* .
+
+$ composer require laravel/ui:^2.4 // auth part1
+$ php artisan ui vue --auth         // auth part2
+
+$ npm install
+$ npm audit fix --force
+$ npm install --save-dev cross-env // windows
